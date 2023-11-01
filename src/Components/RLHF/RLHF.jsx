410d508473f61a296cc964998e662e7ec96b5dd8
dberi786 <111137011+dberi786@users.noreply.github.com>
2023-11-01 14:14:50 +0530

    Update RLHF.jsx

--- src/Components/RLHF/RLHF.jsx
+++ src/Components/RLHF/RLHF.jsx
@@ -41,16 +41,7 @@ const RLHF = () => {
       <Header />
       <SideBar />
       <div
-        style={{
-          width: "1400px",
-          marginLeft: "110px",
-          height: "82px",
-          marginTop: "140px",
-          backgroundColor: "#FFFFFF",
-          borderWidth: "2px",
-          borderRadius: "20px",
-          boxShadow: "2px 2px 4px rgba(0.6, 0.6, 0.6, 0.6)",
-        }}
+        className="sidebar"
       >
         <p
           style={{
